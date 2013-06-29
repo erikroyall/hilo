8d563dd166a861852d1d59fc6d9943046cc05281
Erik Royall <erikroyall@hotmail.com>
2013-06-30 05:10:05 +0530

    .map(), .one() helpers fix

--- src/dom/helpers.js
+++ src/dom/helpers.js
@@ -1,13 +1,19 @@
   // Helper Functions
 
   dom.prototype.each = function (fn) {
-    var _i, _t;
-
-    for (_i = 0, _t = this.length; _i < _t; _i+=1) {
-      fn(this[_i]);
-    }
+    this.map(fn);
+    return this;
   };
+
+  dom.prototype.map = function () {
+    var results = [], _i;
+    for (_i = 0; _i < this.length; _i++) {
+      results.push(callback.call(this, this[_i], _i));
+    }
+    return results;
+  }
   
   dom.prototype.one = function (fn) {
-    fn(this[0]);
+    var m = this.map(callback);
+    return m.length > 1 ? m : m[0];
   };
