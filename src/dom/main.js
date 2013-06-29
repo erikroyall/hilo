61fa0a048a74412c373d29f5d9121f41f97aca6a
Erik Royall <erikroyall@hotmail.com>
2013-06-30 05:12:00 +0530

    Add HTMLElement ($.create()) creation feature

--- src/dom/main.js
+++ src/dom/main.js
@@ -1,9 +1,36 @@
   dom = function (els) {
-    var _i;
+    var _i, _l;
 
-    for (_i = 0; _i < els.length; _i+=1) {
+    for (_i = 0, _l = els.length; _i < _l; _i+=1) {
       this[_i] = els[_i];
     }
 
     this.length = els.length;
   };
+
+  createEl = function (tagName, attrs) {
+    var elm = new dom([document.createElement(tagName)]), key;
+
+    if (attrs) {
+      if (attrs.className) {
+        el.addClass(className);
+        delete attrs.className;
+      }
+
+      if (attrs.text) {
+        el.text(attrs.text);
+        delete attrs.text;
+      }
+
+      for (key in attrs) {
+        if(attrs.hasOwnProperty(key)) {
+          el.attr(key, attr['key']);
+        }
+      }
+    }
+
+    return elm;
+  };
+
+  hilo.create = createEl;
+  
