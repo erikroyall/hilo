b0037de1baefe202ed1076bbfd272f041aff166a
Erik Royall <erikroyall@hotmail.com>
2013-06-30 05:14:53 +0530

    Widen compatibility of .on() and .off() event methods

--- src/evt/helpers.js
+++ src/evt/helpers.js
@@ -1,26 +1,46 @@
   
   // Events
 
-  dom.prototype.on = function (evt, fn) {
-    this.each(function(el) {
-      try {
-        el.addEventListener(evt, fn);
-      } catch (e) {
-        try {
-          el.addEvent(evt, fn);
-        } catch (e) {
-          el['on' + evt] = fn;
-        }
-      }
-    });
+  dom.prototype.on = (function () {
+    if (document.addEventListener) {
+      return function (evt, fn) {
+        return this.forEach(function (el) {
+          el.addEventListener(evt, fn, false);
+        });
+      };
+    } else if (document.attachEvent)  {
+      return function (evt, fn) {
+        return this.forEach(function (el) {
+          el.attachEvent("on" + evt, fn);
+        });
+      };
+    } else {
+      return function (evt, fn) {
+        return this.forEach(function (el) {
+          el["on" + evt] = fn;
+        });
+      };
+    }
+  }());
 
-    return new dom(this);
-  };
-
-  dom.prototype.off = function (evt) {
-    this.each(function(el) {
-      el.removeEventListener(evt);
-    });
-
-    return new dom(this);
-  };
+  dom.prototype.off = (function () {
+    if (document.removeEventListener) {
+      return function (evt, fn) {
+        return this.forEach(function (el) {
+          el.removeEventListener(evt, fn, false);
+        });
+      };
+    } else if (document.detachEvent)  {
+      return function (evt, fn) {
+        return this.forEach(function (el) {
+          el.detachEvent("on" + evt, fn);
+        });
+      };
+    } else {
+      return function (evt, fn) {
+        return this.forEach(function (el) {
+          el["on" + evt] = null;
+        });
+      };
+    }
+  }());
