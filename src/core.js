46aa7611189176bdcb632f927a52f12935c525ad
Erik Royall <erikroyall@hotmail.com>
2013-06-30 13:23:50 +0530

    Create a new Private API select() method for selecting elements

--- src/core.js
+++ src/core.js
@@ -1,32 +1,40 @@
   var hilo          // Public API
     , Dom           // DOM Manipulation Methods
+    , select        
     , feature = {}  // Feature Detection
     , createEl;     // Create an Element
 
-  hilo = function (input, root) {
+  select = function (sel, root) {
     var els, c, rt;
 
     rt = root || document;
 
-    if (typeof input === 'string') { // Selector String
-      if (input.split(" ").length === 1) {
-        c = input.slice(0,1);
-        switch(c) {
-          case "#":
-            els = [rt.getElementById(input.substr(0,input.length))];
-            break;
-          case ".":
-            els = rt.getElementsByClassName(input);
-            break;
-          default:
-            els = rt.getElementsByTagName(input);
-            break;
-        }
-      } else {
-        els = rt.querySelectorAll(input);
+    if(sel.split(" ").length === 1) {
+      c = sel.slice(0,1);
+      switch(c) {
+        case "#":
+          els = [rt.getElementById(sel.substr
+          break;
+        case ".":
+          els = rt.getElementsByClassName(sel);
+          break;
+        case "*":
+          els = document.all;
+          break;
+        default:
+          els = rt.getElementsByTagName(sel);
+          break;
       }
+    } else {
+      els = document.querySelectorAll(sel);
+    }
 
-      return new Dom(els);
+    return els;
+  };
+
+  hilo = function (input, root) {
+    if (typeof input === 'string') {
+      return new Dom(select(input, root));
     } else if (typeof input === 'function') { // Function
       document.onreadystatechange = function () {
         if (document.readyState === 'complete') {
