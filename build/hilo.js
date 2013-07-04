ce883ee9ca9b636fb04ebf82b5ceb4c85bda1007
Erik Royall <erikroyall@hotmail.com>
2013-07-05 04:40:30 +0530

    Make 0.1.0-pre-dev-beta-2 default version

--- build/hilo.js
+++ build/hilo.js
@@ -1,10 +1,10 @@
-/*! Hilo - v0.1.0 - 2013-06-30
+/*! Hilo - v0.1.0 - 2013-07-05
  *  http://erikroyall.github.com/hilo/
  *  Copyright (c) 2013 Erik Royall and Hilo contributors
  *  Licensed under MIT (see LICENSE-MIT) 
  */
 
-window.hilo = (function () {
+window.Hilo = (function () {
 
   "use strict";
   
@@ -12,11 +12,15 @@ window.hilo = (function () {
     , Dom           // DOM Manipulation Methods
     , select        
     , feature = {}  // Feature Detection
-    , createEl;     // Create an Element
+    , hiloAjax      // AJAX Func.
+    , createEl      // Create an Element
+    , Test;
 
   /**
    * Selects and returns elements based on selector given
    *
+   * @method select
+   * @private
    * @param String sel selector
    * @param HTMLElement root root element
    * @return NodeList Array of HTMLElements
@@ -78,7 +82,103 @@ window.hilo = (function () {
     }
   };
 
-  Dom = function (els) {
+hilo.test = function (con) {
+    return new Test(con);
+  };
+
+  /**
+   * Main Test Class
+   *
+   * @class Test
+   * @private
+   * @constructor
+   * @param any con The whatever to be tested
+   * @param boolean neg To negate
+   * @return void
+   */
+
+  Test = function (con, neg) {
+    this.con = con;
+    if (neg) {
+      this.neg = true;
+    }
+  };
+
+
+Test.prototype.ifEquals = function (tw) {
+    var val = this.con === tw;
+    return this.neg ? !val : val;
+  };
+
+  /**
+   * Tests if contains
+   *
+   * @method ifContains
+   * @param any tw Comparative
+   * @return boolean if contains
+   */
+
+  Test.prototype.ifContains = function (tw) {
+    var ifString = this.con.split(tw).length === 1 ? false : true;
+    if (typeof tw === 'string' && typeof this.con === 'object' && this.con.length) {
+
+    } else if (typeof tw === 'string' && typeof this.con === 'string') {
+      return this.neg ? !ifString : ifString;
+    }
+  };
+
+  /**
+   * Tests if it is the same object
+   *
+   * @method ifIs
+   * @param any tw Comparative
+   * @return boolean If it is
+   */
+  
+  Test.prototype.ifIs = function (tw) {
+    var val = this.con === tw;
+    return this.neg ? !val : val;
+  };
+
+hiloAjax = function (config) {
+    var xhr;
+
+    if (window.XMLHttpRequest) {
+      xhr = new window.XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+      xhr = new window.ActiveXObject('Microsoft.XMLHTTP');
+    }
+
+    if (!config.url) {
+      throw new TypeError("url parameter not provided to hilo.ajax");
+    }
+
+    config.async = config.async ? config.async : true;
+    config.username = config.username ? config.username : null;
+    config.password = config.password ? config.password : null;
+
+    if(!config.contentType) {
+      config.contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
+    }
+
+    xhr.onreadystatechange = function () {
+      if (config.callback) {
+        config.callback(xhr);
+      }
+    };
+
+    if (config.method === 'POST') {
+      xhr.open('POST', config.url, config.async, config.username, config.password);
+      xhr.send(config.data);
+    } else {
+      xhr.open('GET', config.url + (config.data ? "+" + config.data : ''), config.async, config.username, config.password);
+      xhr.send();
+    }
+  };
+
+  hilo.ajax = hiloAjax;
+
+Dom = function (els) {
     var _i, _l;
 
     for (_i = 0, _l = els.length; _i < _l; _i+=1) {
@@ -138,15 +238,15 @@ window.hilo = (function () {
   // Element Selections
 
   Dom.prototype.first = function () {
-    return this[0];
+    return new Dom([this[0]]);
   };
   
   Dom.prototype.last = function () {
-    return this[this.length - 1];
+    return new Dom([this[this.length - 1]]);
   };
   
   Dom.prototype.el = function (place) {
-    return this[place];
+    return new Dom([this[place - 1]]);
   };
   
   Dom.prototype.children = function (sel) {
@@ -159,10 +259,21 @@ window.hilo = (function () {
     return new Dom(els);
   };
 
+  Dom.prototype.rel = function (sul) {
+    var els = [];
 
-  // Manipulation
+    this.each(function (el) {
+      els.push(el[sul]);
+    });
 
-  Dom.prototype.html = function (htmlCode) {
+    return new Dom(els);
+  };
+
+  Dom.prototype.next = function () {
+    this.rel('nextSibling');
+  };
+
+Dom.prototype.html = function (htmlCode) {
     if (htmlCode) {
       this.each(function(el) {
         el.innerHTML = htmlCode;
@@ -173,7 +284,17 @@ window.hilo = (function () {
         return el.innerHTML;
       });
     }
+
+    return new Dom(this);
   };
+
+  /**
+   * Changes innerText of selected els
+   *
+   * @method text
+   * @param string texy The text Code to be set
+   * @return object
+   */
 
   Dom.prototype.text = function (text) {
     if (text) {
@@ -188,22 +309,51 @@ window.hilo = (function () {
     }
   };
   
+  /**
+   * Appends to innerHTML of selected els
+   *
+   * @method append
+   * @param string html The HTML Code to be appended
+   * @return object
+   */
+
   Dom.prototype.append = function (html) {
     this.each(function (el) {
       el.innerHTML += html;
     });
   };
   
+  /**
+   * Appends to innerText if selected els
+   *
+   * @method appendText
+   * @param string text The test to be appended
+   * @return object
+   */
+
   Dom.prototype.appendText = function (text) {
     this.each(function (el) {
       el.innerText += text;
     });
+
+    return new Dom(this);
   };
   
+  Dom.prototype.value = function (val) {
+    if (val) {
+      this.each(function (el) {
+        el.value = val;
+      });
 
-  // Classes and IDs
+      return new Dom(this);
+    } else {
+      this.one(function (el) {
+        return el.value;
+      });
+    }
+  };
 
-  Dom.prototype.id = function (id) {
+Dom.prototype.id = function (id) {
     if(id) {
       this.each(function(el) {
         el.id = id;
@@ -216,6 +366,14 @@ window.hilo = (function () {
       });
     }
   };
+
+  /**
+   * Adds a class of selected els
+   *
+   * @method addClass
+   * @param {String|Array} className The class name of list of class names
+   * @return object
+   */
 
   Dom.prototype.addClass = function (className) {
     this.each(function (el) {
@@ -237,10 +395,21 @@ window.hilo = (function () {
     });
   };
   
+  Dom.prototype.attr = function (name, val) {
+    if(val) {
+      this.each(function(el) {
+        el.setAttribute(name, val);
+      });
 
-  // CSS
+      return new Dom(this);
+    } else {
+      this.one(function (el) {
+        return el[name];
+      });
+    }
+  };
 
-  Dom.prototype.css = function (prop, value) {
+Dom.prototype.css = function (prop, value) {
     if (value) {
       this.each(function (el) {
         el.style[prop] = value;
@@ -254,6 +423,14 @@ window.hilo = (function () {
     }
   };
   
+  /**
+   * Set or return width of selected el(s)
+   *
+   * @method width
+   * @param {String|Number} prop width
+   * @return {object|String}
+   */
+
   Dom.prototype.width = function (width) {
     if (width) {
       this.each(function (el) {
@@ -268,6 +445,14 @@ window.hilo = (function () {
     }
   };
   
+  /**
+   * Set or return height of selected el(s)
+   *
+   * @method height
+   * @param {String|Number} height height
+   * @return {object|String}
+   */
+
   Dom.prototype.height = function (height) {
     if (height) {
       this.each(function (el) {
@@ -282,6 +467,14 @@ window.hilo = (function () {
     }
   };
   
+  /**
+   * Set or return color of selected el(s)
+   *
+   * @method color
+   * @param String color color
+   * @return {object|String}
+   */
+
   Dom.prototype.color = function (color) {
     if (color) {
       this.style('color', color);
@@ -294,6 +487,14 @@ window.hilo = (function () {
     }
   };
   
+  /**
+   * Set or return background color of selected el(s)
+   *
+   * @method backgroundColor
+   * @param String backgroundColor background color
+   * @return {object|String}
+   */
+
   Dom.prototype.backgroundColor = function (backgroundColor) {
     if (backgroundColor) {
       this.style('background-color', backgroundColor);
@@ -306,6 +507,14 @@ window.hilo = (function () {
     }
   };
   
+  /**
+   * Set or return background prop of selected el(s)
+   *
+   * @method background
+   * @param String background background
+   * @return {object|String}
+   */
+
   Dom.prototype.background = function (background) {
     if (background) {
       this.style('background', background);
@@ -318,6 +527,14 @@ window.hilo = (function () {
     }
   };
   
+  /**
+   * Set or return margin of selected el(s)
+   *
+   * @method margin
+   * @param {String|Number} margin margin
+   * @return {object|String}
+   */
+
   Dom.prototype.margin = function (margin) {
     if (margin) {
       this.style('margin', margin);
@@ -330,6 +547,14 @@ window.hilo = (function () {
     }
   };
   
+  /**
+   * Set or return padding of selected el(s)
+   *
+   * @method padding
+   * @param {String|Number} padding padding
+   * @return {object|String}
+   */
+
   Dom.prototype.padding = function (padding) {
     if (padding) {
       this.style('padding', padding);
@@ -342,6 +567,14 @@ window.hilo = (function () {
     }
   };
   
+  /**
+   * Set or return font size of selected el(s)
+   *
+   * @method fontSize
+   * @param {String|Number} fontSize font size
+   * @return {object|String}
+   */
+
   Dom.prototype.fontSize = function (fontSize) {
     if (fontSize) {
       this.style('font-size', fontSize);
@@ -354,20 +587,17 @@ window.hilo = (function () {
     }
   };
 
-
-  Dom.prototype.get = function () {
+Dom.prototype.get = function () {
     var els = [];
 
     this.each(function (el) {
-      console.dir(els);
-      els = [].push(el);
+      els.push(el);
     });
 
     return els;
   };
   
-  // Events
-
+  
   Dom.prototype.on = (function () {
     if (document.addEventListener) {
       return function (evt, fn) {
@@ -411,6 +641,101 @@ window.hilo = (function () {
       };
     }
   }());
+
+  
+  Dom.prototype.ready = function (fn) {
+    this.each(function (el) {
+      el.onreadystatechange = function () {
+        if (el.readyState = 'complete') {
+          fn();
+        }
+      };
+    });
+  };
+
+  Dom.prototype.click = function (fn) {
+    this.on('click', fn);
+  };
+
+  Dom.prototype.hover = function (fn) {
+    this.on('hover', fn);
+  };
+
+  Dom.prototype.focus = function (fn) {
+    this.on('focus', fn);
+  };
+
+  Dom.prototype.drag = function (fn) {
+    this.on('drag', fn);
+  };
+
+  Dom.prototype.dragenter = function (fn) {
+    this.on('dragenter', fn);
+  };
+
+  Dom.prototype.dragend = function (fn) {
+    this.on('dragend', fn);
+  };
+
+  Dom.prototype.dragleave = function (fn) {
+    this.on('dragleave', fn);
+  };
+
+  Dom.prototype.dragover = function (fn) {
+    this.on('dragover', fn);
+  };
+
+  Dom.prototype.dragstart = function (fn) {
+    this.on('dragstart', fn);
+  };
+
+  Dom.prototype.drop = function (fn) {
+    this.on('drop', fn);
+  };
+
+  Dom.prototype.keyup = function (fn) {
+    this.on('keyup', fn);
+  };
+
+  Dom.prototype.keypress = function (fn) {
+    this.on('keypress', fn);
+  };
+
+  Dom.prototype.keydown = function (fn) {
+    this.on('keydown', fn);
+  };
+
+  Dom.prototype.load = function (fn) {
+    this.on('load', fn);
+  };
+
+  Dom.prototype.mouseup = function (fn) {
+    this.on('mouseup', fn);
+  };
+
+  Dom.prototype.mouseover = function (fn) {
+    this.on('mouseover', fn);
+  };
+
+  Dom.prototype.mousedown = function (fn) {
+    this.on('mousedown', fn);
+  };
+
+  Dom.prototype.mousewheel = function (fn) {
+    this.on('mousewheel', fn);
+  };
+
+  Dom.prototype.change = function (fn) {
+    this.on('change', fn);
+  };
+
+  Dom.prototype.blur = function (fn) {
+    this.on('blur', fn);
+  };
+
+  Dom.prototype.submit = function (fn) {
+    this.on('submit', fn);
+  };
 
 
   // Effects
@@ -475,89 +800,153 @@ window.hilo = (function () {
   };
 
 
-  feature = {
-    applicationcache: (function () {
-      return !!window.applicationCache;
-    }()),
-    audiopreload: (function () {
-      return 'preload' in document.createElement('audio');
-    }()),
-    canvas: (function () {
-      return !!document.createElement('canvas').getContext;
-    }()),
-    classlist: (function () {
-      return 'classList' in document.createElement('p');
-    }()),
-    es6: (function () {
-      return typeof String.prototype.contains === 'function';
-    }()),
-    geolocation: (function () {
-      return 'geolocation' in window.navigator;
-    }()),
-    indexeddb: (function () {
-      return !!(window.indexedDB && window.IDBKeyRange && window.IDBTransaction);
-    }()),
-    input: {
+  feature = (function () {
+    var i = document.createElement("input");
 
-    },
-    localstorage: (function () {
-      try {
-        return 'localStorage' in window && window['localStorage'] !== null && !!window.localStorage.setItem;
-      } catch(e){
-        return false;
-      }
-    }()),
-    microdata: (function () {
-      return 'getItems' in document;
-    }()),
-    placeholder : (function () {
-      return 'placeholder' in document.createElement('input');
-    }()),
-    template: (function () {
-      return 'content' in document.createElement('template');
-    }()),
-    video: (function () {
-      try {
-        return !!document.createElement('video').canPlayType;
-      } catch (e) {
-        return false;
-      }
-    }()),
-    h264: (function () {
-      var v = document.createElement("video");
-      try {
-        return v.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
-      } catch (e) {
-        return false;
-      }
-    }()),
-    webm: (function () {
-      var v = document.createElement("video");
-      try {
-        return v.canPlayType('video/webm; codecs="vp8, vorbis"');
-      } catch (e) {
-        return false;
-      }
-    }()),
-    ogg: (function () {
-      var v = document.createElement("video");
-      try {
-        return v.canPlayType('video/ogg; codecs="theora, vorbis"');
-      } catch (e) {
-        return false;
-      }
-    }()),
-    webaudio: (function () {
-      return !!(window.webkitAudioContext || window.AudioContext);
-    }()),
-    webworkers: (function () {
-      return !!window.Worker;
-    }())
-  };
+    return {
+      applicationcache: (function () {
+        return !!window.applicationCache;
+      }()),
+      audiopreload: (function () {
+        return 'preload' in document.createElement('audio');
+      }()),
+      canvas: (function () {
+        return !!document.createElement('canvas').getContext;
+      }()),
+      classlist: (function () {
+        return 'classList' in document.createElement('p');
+      }()),
+      es6: (function () {
+        return typeof String.prototype.contains === 'function';
+      }()),
+      geolocation: (function () {
+        return 'geolocation' in window.navigator;
+      }()),
+      history: (function () {
+        return !!(window.history && history.pushState);
+      }()),
+      indexeddb: (function () {
+        return !!(window.indexedDB && window.IDBKeyRange && window.IDBTransaction);
+      }()),
+      input: {
+        autofocus: (function () {
+          return 'autofocus' in i;
+        }()),
+        placeholder: (function () {
+          return 'placeholder' in i;
+        }()),
+        type: {
+          color: (function () {
+            i.setAttribute('type', 'color');
+            return i.type !== 'text';
+          }()),
+          date: (function () {
+            i.setAttribute('type', 'date');
+            return i.type !== 'text';
+          }()),
+          datetime: (function () {
+            i.setAttribute('type', 'datetime');
+            return i.type !== 'text';
+          }()),
+          datetimeLocal: (function () {
+            i.setAttribute('type', 'datetime-local');
+            return i.type !== 'text';
+          }()),
+          email: (function () {
+            i.setAttribute('type', 'email');
+            return i.type !== 'text';
+          }()),
+          month: (function () {
+            i.setAttribute('type', 'month');
+            return i.type !== 'text';
+          }()),
+          number: (function () {
+            i.setAttribute('type', 'number');
+            return i.type !== 'text';
+          }()),
+          range: (function () {
+            i.setAttribute('type', 'range');
+            return i.type !== 'text';
+          }()),
+          search: (function () {
+            i.setAttribute('type', 'search');
+            return i.type !== 'text';
+          }()),
+          tel: (function () {
+            i.setAttribute('type', 'tel');
+            return i.type !== 'text';
+          }()),
+          time: (function () {
+            i.setAttribute('type', 'time');
+            return i.type !== 'text';
+          }()),
+          week: (function () {
+            i.setAttribute('type', 'week');
+            return i.type !== 'text';
+          }())
+        }
+      },
+      localstorage: (function () {
+        try {
+          return 'localStorage' in window && window['localStorage'] !== null && !!window.localStorage.setItem;
+        } catch(e){
+          return false;
+        }
+      }()),
+      microdata: (function () {
+        return 'getItems' in document;
+      }()),
+      template: (function () {
+        return 'content' in document.createElement('template');
+      }()),
+      video: (function () {
+        try {
+          return !!document.createElement('video').canPlayType;
+        } catch (e) {
+          return false;
+        }
+      }()),
+      h264: (function () {
+        var v = document.createElement("video");
+        try {
+          return v.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
+        } catch (e) {
+          return false;
+        }
+      }()),
+      webm: (function () {
+        var v = document.createElement("video");
+        try {
+          return v.canPlayType('video/webm; codecs="vp8, vorbis"');
+        } catch (e) {
+          return false;
+        }
+      }()),
+      ogg: (function () {
+        var v = document.createElement("video");
+        try {
+          return v.canPlayType('video/ogg; codecs="theora, vorbis"');
+        } catch (e) {
+          return false;
+        }
+      }()),
+      webaudio: (function () {
+        return !!(window.webkitAudioContext || window.AudioContext);
+      }()),
+      webworkers: (function () {
+        return !!window.Worker;
+      }())
+    };
+  }());
 
   hilo.feature = feature;
 
 
+  hilo.noConflict = function () {
+    delete window.$;
+  };
+  
+  
   window.$ = hilo;
 
   return hilo;
