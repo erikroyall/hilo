44d99e83cb0d04742da0929dd57d7295c4f197c9
Erik Royall <erikroyall@hotmail.com>
2013-07-17 22:56:48 +0530

    Few deletions and fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,18 @@
 /*global module:false*/
+
 module.exports = function(grunt) {
 
-  // Project configuration.
+  "use strict";
+  
   grunt.initConfig({
-    // Metadata.
+    
     pkg: grunt.file.readJSON('package.json'),
     banner: '/* <%= pkg.title %> - <%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       ' * http://erikroyall.github.com/<%= pkg.name %>/\n' +
       ' * Copyright (c) 2013 Erik Royall and <%= pkg.title %> contributors\n' +
       ' * Licensed under <%= pkg.license %> (see LICENSE-MIT) \n */\n\n',
-    // Task configuration.
+    
     concat: {
       options: {
         banner: '<%= banner %>',
@@ -87,14 +89,11 @@ module.exports = function(grunt) {
     }
   });
 
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  // grunt.loadNpmTasks('grunt-contrib-copy');
-
-  // Default task.
+  
   grunt.registerTask('default', ['concat', 'uglify:hilo', 'jshint', 'watch']);
   grunt.registerTask('release', ['concat']);
 
--- doc/dom/addClass.md
+++
@@ -1,5 +0,0 @@
-.addClass()
-===========
-
-*Adds class(es) to selected elements*
-
