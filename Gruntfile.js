e0834d9cf2e90edec6c9fa62c1fb198b50cbb030
Erik Royall <erikroyall@hotmail.com>
2013-08-08 19:51:14 +0530

    Hello

---
+++ Gruntfile.js
@@ -0,0 +1,136 @@
+/*global module:false*/
+
+module.exports = function(grunt) {
+
+  "use strict";
+
+  // Run: "grunt --no-color --force" without the quotes
+  // and keep it clean
+
+  var itsbeen = " * "
+    , st = new Date(1375182296792)
+    , ct = new Date()
+    , diff = {
+      y: ct.getFullYear() - st.getFullYear(),
+      m: ct.getMonth() - st.getMonth(),
+      d: ct.getDate() - st.getDate()
+    };
+
+  itsbeen += "Project started before ";
+  itsbeen += (diff.y === 0) ? "" : diff.y === 1 ? 1 + " year, " : ((diff.y < 0 ? 30 - Math.abs(diff.y) : diff.y) + " years") + " and "; 
+  itsbeen += (diff.m === 0) ? "" : diff.m === 1 ? 1 + " month and " : ((diff.m < 0 ? 30 - Math.abs(diff.m) : diff.m) + " months") + " and "; 
+  itsbeen += (diff.d === 0) ? "" : diff.d === 1 ? 1 + " day " : ((diff.d < 0 ? 30 - Math.abs(diff.d) : diff.d) + " days"); 
+  itsbeen += "\n";
+  
+  grunt.initConfig({
+    
+    pkg: grunt.file.readJSON("package.json"),
+    banner: "/*! \n * <%= pkg.title %> - <%= pkg.version %> - " +
+      "<%= grunt.template.today(\"yyyy-mm-dd\") %>\n" +
+      itsbeen +
+      " * http://erikroyall.github.com/<%= pkg.name %>/\n" +
+      " * Copyright (c) 2013 Erik Royall\n" +
+      " * Licensed under <%= pkg.license %> (see LICENSE-MIT) \n */\n\n",
+    
+    concat: {
+      options: {
+        banner: "<%= banner %>",
+        stripBanners: true
+      },
+      dist: {
+        src: [
+          "src/start.js",
+          "src/detect/browser.js",
+          "src/detect/feature.js",
+          "src/dom/sizzle.js",
+          "src/es/util.js",
+          "src/core.js",
+          "src/test/main.js",
+          "src/test/comp.js",
+          "src/shim/shims.js",
+          "src/ajax/main.js",
+          "src/dom/main.js",
+          "src/dom/helpers.js",
+          "src/dom/els.js",
+          "src/dom/manp.js",
+          "src/dom/class-id.js",
+          "src/dom/css.js",
+          "src/dom/misc.js",
+          "src/evt/helpers.js",
+          "src/evt/simple.js",
+          "src/fx/simple.js",
+          "src/detect/classify.js",
+          "src/evt/keymap.js",
+          "src/ext.js",
+          "src/end.js"
+          ],
+        dest: "build/<%= pkg.name %>-dev.js"
+      }
+    },
+    uglify: {
+      hilo: {
+        files: {
+          "build/<%= pkg.name %>-dev.min.js" : ["build/<%= pkg.name %>-dev.js"]
+        }
+      }
+    },
+    jasmine: {
+      options: {
+        helpers: "build/hilo-dev.js",
+        junit: {
+          path: "junit"
+        }
+      },
+      hilo: {
+        src: "test/spec/**/*.spec.js"
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        laxcomma: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          console: true
+        }
+      },
+      gruntfile: {
+        src: "Gruntfile.js"
+      },
+      hilo: {
+        src: "build/<%= pkg.name %>-dev.js"
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: "<%= jshint.gruntfile.src %>",
+        tasks: ["jshint:gruntfile", "concat", "uglify:hilo", "jshint:hilo", "jasmine:hilo"]
+      },
+      hilo: {
+        files: "<%= concat.dist.src %>",
+        tasks: ["concat", "uglify:hilo", "jshint:hilo", "jasmine:hilo"]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-jasmine");
+
+  
+  grunt.registerTask("default", ["concat", "uglify:hilo", "jasmine:hilo", "jshint", "watch"]);
+  grunt.registerTask("release", ["concat"]);
+
+};
---
+++ README.md
@@ -0,0 +1,75 @@
+Hilo
+====
+_0.1.0 pre dev beta_ - **0.1.0-pre-dev-beta-7**
+
+Hilo is a lightning fast Open Source JavaScript Framework and library that makes developing awesome websites easy. With Hilo, you won't have to worry about speed or backwards compatibility. All these are provided to you. It all depends on how you make use of it.
+
+## Features
+
+- DOM Manipulation
+- Feature Detection
+- Events
+- AJAX
+
+## Usage
+
+**Note: Hilo is currently under development. Even a public beta version is not yet released. The current version is _0.1.0 pre dev beta_ .**
+
+Add Hilo in the `<head>` or `<body>` section of your page.
+
+```html
+  <head>
+    ...
+    <script type="text/javascript" src="hilo.js"></script>
+  </head>
+  ...
+```
+
+Create another `<script>` and write Hilo code there.
+
+```html
+<script>
+  $(function () {
+    $('div').click(function () {
+      $(this).toggle();
+    });
+  });
+</script>
+```
+
+## About Hilo
+
+### Name
+
+Well, it's a weird name. The reason behind choosing this name is
+that the author of Hilo, Erik Royall, likes to write *Hilo Wald*
+programs instead of _Hello World_ programs. And when he got the
+idea of writing a JavaScript library he used the first word in
+*Hilo Wald* as the name for the library. 
+_Note: The name **Wald** will undoubtedly be used as a name for_
+_his next project so..._
+
+### License
+
+Copyright (c) 2013 **Erik Royall**
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
