9a414f37b1b01ca68fc087bf84685f2b8ab8ba89
Erik Royall <erikroyall@hotmail.com>
2013-08-09 15:04:03 +0530

    AJAX Optimizations

--- src/ajax/main.js
+++ src/ajax/main.js
@@ -5,26 +5,26 @@
   hiloAjax = function (config) {
       
     /*
-    
-      config:
-      - method: HTTP Method "GET" or "POST" (default: "POST")
-      - url: The file to send request
-      - async: Whether to perform an asynchronous request (default: true)
-      - response: Response type "text" or "XML"
-      - Event functions
-        - callback: fn to be exec. on readystatechange
-        - complete
-        - error
-        - timeout
-        - success
-        - notfound
-        - forbidden
-      - username
-      - password
-      - contentType
-    
-    */
-
+     *
+     * config:
+     *  
+     * - method: HTTP Method "GET" or "POST" (default: "POST")
+     * - url: The file to send request
+     * - async: Whether to perform an asynchronous request (default: true)
+     * - response: Response type "text" or "XML"
+     * - Event functions
+     *   - callback: fn to be exec. on readystatechange
+     *   - complete
+     *   - error
+     *   - timeout
+     *   - success
+     *   - notfound
+     *   - forbidden
+     * - username
+     * - password
+     * - contentType
+     *
+     */
     
     var xhr;
 
@@ -38,10 +38,16 @@
       throw new TypeError("url parameter not provided to hilo.ajax");
     }
 
+    // Set defaults
+
+    // Asynchronous requests are preferred
     config.async = config.async || true;
+
+    // Authentication params
     config.username = config.username || null;
     config.password = config.password || null;
 
+    // contentType application/x-www-form-urlencoded; charset=UTF-8 is preferred
     config.contentType = config.contentType || "application/x-www-form-urlencoded; charset=UTF-8";
 
     xhr.onreadystatechange = function () {
@@ -87,32 +93,37 @@
 
     xhr.timeout = config.timeout;
 
-    if (config.method.trim().toUpperCase() === "POST") {
-      xhr.open(
-        "POST",
-        config.url,
-        config.async,
-        config.username,
-        config.password
-      );
-      xhr.send(config.data);
-    } else if (config.method.trim().toUpperCase() === "GET") {
-      xhr.open(
-        "GET",
-        config.url + (config.data ? "+" + config.data : ""),
-        config.async,
-        config.username,
-        config.password
-      );
-      xhr.send();
+    if (typeof config.method === "string") {
+      if (config.method.trim().toUpperCase() === "POST") {
+        xhr.open(
+          "POST",
+          config.url,
+          config.async,
+          config.username,
+          config.password
+        );
+
+        xhr.send(config.data);
+      } else if (config.method.trim().toUpperCase() === "GET") {
+        xhr.open(
+          "GET",
+          config.url + (config.data ? "+" + config.data : ""),
+          config.async,
+          config.username,
+          config.password
+        );
+
+        xhr.send();
+      }
     } else {
       xhr.open(
         config.method.trim().toUpperCase(),
         config.url + (config.data ? "+" + config.data : ''),
-        (config.async ? config.async: true),
-        (config.username ? config.username : null),
-        (config.password ? config.password : null)
+        config.async,
+        config.username,
+        config.password
       );
+
       xhr.send();
     }
   };
